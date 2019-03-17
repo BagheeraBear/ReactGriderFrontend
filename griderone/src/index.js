bae4629ed584e6aa899bc8e81d15bb08429f4e3e
Ulf Swedin <ulfswedin@Ulfs-MacBook-Air.local>
2019-03-17 16:47:35 +0100

    HTML to JSX

--- griderone/src/index.js
+++ griderone/src/index.js
@@ -2,14 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // a function component
+// HTML > JSX Excercise
 const App = function() {
   return (
     <div>
       <h1>This is my cool function-Component</h1>
       <p>And som text to show under the header</p>
       <p>Lorem ipsum baby</p>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: blue; color: white}}>Submit</button>
+
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+// Next: Props
+// Classbased Components
+// State
+// Lifecycle
+// Forms and Events
+// API
+// Redux
+// React Router
+// Authentication
+// RESP API App
+
