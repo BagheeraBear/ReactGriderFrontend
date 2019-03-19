1301a105789597ddfe3b20f2954f993b9ec6f547
Ulf Swedin <ulf@swedin.nu>
2019-03-19 13:37:15 +0100

    More Components and Props

--- components/src/index.js
+++ components/src/index.js
@@ -1,23 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui cointainer comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6.00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author="Sammy"
+        timeAgo="Today at 4:00PM"
+        avatar={faker.image.avatar()}
+        commentText="Great!"
+      />
+      <CommentDetail
+        author="Bunny"
+        timeAgo="Yesterday at 02.15AM"
+        avatar={faker.image.avatar()}
+        commentText="Great again!"
+      />
+      <CommentDetail
+        author="Kimmy"
+        timeAgo="Today at 05.25"
+        avatar={faker.image.avatar()}
+        commentText="Truly Great!"
+      />
+      <CommentDetail
+        author="Sander"
+        timeAgo="Todat at 01.00AM"
+        avatar={faker.image.avatar()}
+        commentText="Not only Great, but the Best!!"
+      />
     </div>
   );
 };
@@ -25,3 +37,6 @@ const App = () => {
 ReactDOM.render(<App />, document.querySelector("#root"));
 
 // https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css
+
+// Next: Fix images and break it up
+// faker.js
