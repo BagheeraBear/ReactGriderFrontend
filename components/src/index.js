ad2aa612325bd1f93f53145988811c413bc341dc
Ulf Swedin <ulf@swedin.nu>
2019-03-20 14:36:24 +0100

    Adding seasons app

--- components/src/index.js
+++ components/src/index.js
@@ -2,34 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui cointainer comments">
-      <CommentDetail
+      <ApprovalCard>
+        <CommentDetail
         author="Sammy"
         timeAgo="Today at 4:00PM"
         avatar={faker.image.avatar()}
         commentText="Great!"
-      />
-      <CommentDetail
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
         author="Bunny"
         timeAgo="Yesterday at 02.15AM"
         avatar={faker.image.avatar()}
         commentText="Great again!"
-      />
-      <CommentDetail
+        />
+        </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
         author="Kimmy"
         timeAgo="Today at 05.25"
         avatar={faker.image.avatar()}
         commentText="Truly Great!"
-      />
-      <CommentDetail
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
         author="Sander"
         timeAgo="Todat at 01.00AM"
         avatar={faker.image.avatar()}
         commentText="Not only Great, but the Best!!"
-      />
+        />
+      </ApprovalCard>
     </div>
   );
 };
