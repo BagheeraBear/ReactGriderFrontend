cac4d1134f8fe606f895ddbff7fc326332e10bc6
Ulf Swedin <ulf@swedin.nu>
2019-03-21 14:48:32 +0100

    State and Conditionial Rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// into classBased prototypal inheritance
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  // constructor, first called instantly before anything else
+  // super - a reference to the parents contructor-function
+  constructor(props) {
+    super(props);
+    // only time we make a direct assignment!
+    this.state = { latitude: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we called setState!!
+        this.setState({ latitude: position.coords.latitude });
+      },
+      error => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  // React says we have to define render()
+  render() {
+    // update state > immediate re-render
+
+    // Conditional Rendering, 3 scenarios
+
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+
+    return <div>Loading...!</div>;
+  }
+}
+
+// React.Component has methods
+
+// State
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+/* functional
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+  );
+
+  return <div>Hi there</div>;
+};
+*/
