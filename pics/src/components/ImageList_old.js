f7a1762df1c998d209a8d9e1e8cf682c7a924614
Ulf Swedin <ulf@swedin.nu>
2019-04-08 16:21:26 +0200

    CSS Grid

---
+++ pics/src/components/ImageList_old.js
@@ -0,0 +1,10 @@
+import './ImageList.css'
+import React from "react";
+
+class ImageList extends React.Component {
+  render() {
+    return <div className="image-list">{images}</div>;
+  }
+}
+
+export default ImageList;
